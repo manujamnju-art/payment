d002cc2ca643d40b5454d10869e14c73cfa3bc78
manujamnju-art <manujamnju@gmail.com>
2025-11-10 16:04:31 +0530

    Update bouns.py

--- bouns.py
+++ bouns.py
@@ -1,5 +1,5 @@
-salary = float(input("Enter employee salary: ₹"))
+salary = float(input("Enter employee salary:"))
 bonus = salary * 0.10
 total_salary = salary + bonus
-print("Bonus Amount: ₹", bonus)
-print("Total Salary after Bonus: ₹",total_salary)
+print("Bonus Amount:", bonus)
+print("Total Salary after Bonus:",total_salary)
